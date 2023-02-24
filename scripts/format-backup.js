1a9fb2625be77992bcc36bba9cd40d0bbd277c03
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2023-02-24 23:30:13 +0300

    remove

--- scripts/format-backup.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-const filePath = path.join(__dirname, '../backup/current.json');
-
-const content = fs.readFileSync(filePath, 'utf-8');
-
-fs.writeFileSync(filePath, JSON.stringify(JSON.parse(content), null, 2));
